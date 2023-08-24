d2934d8b12d1aeaa355ca45462fdb7811b64d733
Jisoo Yoo <youzysu@gmail.com>
2023-08-24 14:52:56 +0900

    chore: styled-component theme type 추가

---
+++ src/styles/styled.d.ts
@@ -0,0 +1,14 @@
+// TODO: theme 타입 정의
+
+import { designSystem } from "./designSystem";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    color: typeof designSystem.color;
+    filter: typeof designSystem.filter;
+    backdropFilter: typeof designSystem.backdropFilter;
+    radius: typeof designSystem.radius;
+    font: typeof designSystem.font;
+    opacity: typeof designSystem.opacity;
+  }
+}
