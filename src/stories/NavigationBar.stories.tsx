ef62f759753f087421015fffd42fb07d59587c6e
유지수 Jisoo Yoo <youzysu@gmail.com>
2023-08-28 16:21:42 +0900

    chore #16: 스토리북 기본 설정 (#26)

---
+++ src/stories/NavigationBar.stories.tsx
@@ -0,0 +1,9 @@
+import NavigationBar from "@components/NavigationBar";
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta: Meta<typeof NavigationBar> = {
+  component: NavigationBar,
+};
+
+export default meta;
+type Story = StoryObj<typeof NavigationBar>;
