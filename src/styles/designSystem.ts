d1bbcac205ffadb66713262471456b77f5b6fdcb
aaaz425 <aaaz425@naver.com>
2023-08-29 11:45:37 +0900

    chore: filter 추가

--- src/styles/designSystem.ts
+++ src/styles/designSystem.ts
@@ -79,10 +79,14 @@ export const designSystem = {
       "opacity(60%) brightness(0) saturate(100%) invert(20%) sepia(2%) saturate(3198%) hue-rotate(202deg) brightness(93%) contrast(83%)",
     neutralText:
       "invert(21%) sepia(2%) saturate(2651%) hue-rotate(202deg) brightness(95%) contrast(87%)",
-    neutralTextStrong: "",
-    neutralBackground: "",
-    neutralBackgroundWeak: "",
-    neutralBackgroundBold: "",
+    neutralTextStrong:
+      "invert(0%) sepia(100%) saturate(3089%) hue-rotate(210deg) brightness(99%) contrast(109%)",
+    neutralBackground:
+      "invert(100%) sepia(0%) saturate(0%) hue-rotate(299deg) brightness(105%) contrast(101%)",
+    neutralBackgroundWeak:
+      "invert(100%) sepia(100%) saturate(0%) hue-rotate(115deg) brightness(107%) contrast(96%)",
+    neutralBackgroundBold:
+      "invert(100%) sepia(13%) saturate(400%) hue-rotate(180deg) brightness(99%) contrast(89%)",
     neutralBackgroundBlur: "",
     neutralBorder: "",
     neutralBorderStrong: "",
@@ -90,13 +94,17 @@ export const designSystem = {
 
     accentText:
       "invert(100%) sepia(97%) saturate(15%) hue-rotate(110deg) brightness(103%) contrast(102%)",
-    accentTextWeak: "",
-    accentPrimary: "",
-    accentSecondary: "",
+    accentTextWeak:
+      "invert(0%) sepia(90%) saturate(7500%) hue-rotate(167deg) brightness(83%) contrast(109%)",
+    accentPrimary:
+      "invert(38%) sepia(22%) saturate(3963%) hue-rotate(220deg) brightness(85%) contrast(97%)",
+    accentSecondary:
+      "invert(87%) sepia(23%) saturate(4965%) hue-rotate(359deg) brightness(101%) contrast(104%)",
 
     systemWarning:
       "invert(36%) sepia(25%) saturate(6170%) hue-rotate(341deg) brightness(98%) contrast(105%)",
-    systemBackground: "",
+    systemBackground:
+      "invert(100%) sepia(99%) saturate(23%) hue-rotate(3deg) brightness(104%) contrast(100%)",
     systemBackgroundWeak: "",
   },
   backdropFilter: {
