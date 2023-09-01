4b37d593fcc4f932565a9ec6e0488b32aece021a
aaaz425 <aaaz425@naver.com>
2023-09-01 13:42:00 +0900

    feat #36: 홈 FAB 버튼 생성

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,21 @@
 import { ReactComponent as ChevronDownIcon } from "@assets/icon/chevron-down.svg";
 import { ReactComponent as LayoutGridIcon } from "@assets/icon/layout-grid.svg";
+import { ReactComponent as PlusIcon } from "@assets/icon/plus.svg";
 import NavigationBar from "@components/NavigationBar";
 import TopBar from "@components/TopBar";
 import Button from "@components/common/Buttons/Button";
+import { ROUTE_PATH } from "@router/constants";
 import { Page } from "@styles/common";
+import { useNavigate } from "react-router-dom";
 import { styled } from "styled-components";
 
 export default function Home() {
+  const navigate = useNavigate();
+
+  const goToAddProduct = () => {
+    navigate(ROUTE_PATH.new);
+  };
+
   return (
     <Page>
       <TopBar
@@ -23,6 +32,16 @@ export default function Home() {
         rightBtn={<Button color="neutralText" leftIcon={<LayoutGridIcon />} />}
       />
       <Test>상품 목록 리스트</Test>
+      <FAB>
+        <Button
+          size={{ width: 56, height: 56 }}
+          color="accentText"
+          backgroundColor="accentPrimary"
+          radius="half"
+          leftIcon={<PlusIcon />}
+          onClick={goToAddProduct}
+        />
+      </FAB>
       <NavigationBar />
     </Page>
   );
@@ -30,4 +49,12 @@ export default function Home() {
 
 const Test = styled.div`
   height: 2000px;
+`;
+
+const FAB = styled.div`
+  width: 56px;
+  margin-left: auto;
+  position: sticky;
+  right: 24px;
+  bottom: 24px;
 `;
