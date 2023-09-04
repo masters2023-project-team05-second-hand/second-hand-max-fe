3e8bae481c9c4a5ccddd58e98d7e3f4b9493e370
Jisoo Yoo <youzysu@gmail.com>
2023-09-04 11:54:58 +0900

    chore: fe 루트 디렉토리 생성 및 파일 위치 일괄 이동

--- fe/src/api/constants.ts
+++ fe/src/api/constants.ts
@@ -2,7 +2,6 @@ import { ROUTE_PATH } from "@router/constants";
 
 const {
   VITE_APP_API_URL,
-  VITE_PRODUCTION_API_URL,
   VITE_OAUTH_KAKAO_CLIENT_ID,
   VITE_OAUTH_GITHUB_CLIENT_ID,
 } = import.meta.env;
@@ -15,10 +14,7 @@ export const API_PATH = {
   userProfile: "/api/members/profile-image",
 };
 
-export const BASE_API_URL =
-  process.env.NODE_ENV === "production"
-    ? VITE_APP_API_URL
-    : VITE_PRODUCTION_API_URL;
+export const BASE_API_URL = VITE_APP_API_URL;
 
 export const KAKAO_LOGIN_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${VITE_OAUTH_KAKAO_CLIENT_ID}&redirect_uri=${BASE_API_URL}${ROUTE_PATH.auth.kakao}&response_type=code`;
 
