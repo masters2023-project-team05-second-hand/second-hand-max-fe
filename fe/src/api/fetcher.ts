d28b42aeb5734f227b2c97069c21512743969767
Jisoo Yoo <youzysu@gmail.com>
2023-09-06 10:27:49 +0900

    refactor: 401 Authorization Error 토큰 재발급 처리

--- fe/src/api/fetcher.ts
+++ fe/src/api/fetcher.ts
@@ -24,6 +24,9 @@ fetcher.interceptors.request.use(
 fetcher.interceptors.response.use(
   (response) => response,
   (error: AxiosError) => {
+    if (error.response?.status !== 401) {
+      return Promise.reject(error);
+    }
     try {
       const originalRequest = error.config;
 
