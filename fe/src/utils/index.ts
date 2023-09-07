ab4fd3c9379cb9bf3b62357ca1f0f3cd0721605c
aaaz425 <aaaz425@naver.com>
2023-09-07 12:17:00 +0900

    feat #36: 상품 등록 페이지 price 포맷 변경 유틸 함수 구현

--- fe/src/utils/index.ts
+++ fe/src/utils/index.ts
@@ -4,3 +4,16 @@ export const isSameItems = (a: number[], b: number[]) => {
 
   return setA.size === setB.size && [...setA].every((value) => setB.has(value));
 };
+
+export const getFormattedPrice = (inputValue: string) => {
+  const onlyNumber = inputValue.replace(/[^\d]+/g, "");
+  const limitedPrice = onlyNumber.slice(0, 9);
+
+  if (limitedPrice) {
+    const krw = parseInt(limitedPrice).toLocaleString("ko-KR");
+
+    return krw;
+  } else {
+    return "";
+  }
+};
