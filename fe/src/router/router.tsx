f387841023e6c83726b42eda86bcc1de5fbc5ace
aaaz425 <aaaz425@naver.com>
2023-09-07 12:16:05 +0900

    chore #36: 라우팅 경로 수정

--- fe/src/router/router.tsx
+++ fe/src/router/router.tsx
@@ -1,10 +1,10 @@
 import { Auth } from "@pages/Auth";
 import Chat from "@pages/Chat";
-import Category from "@pages/Home/Category";
-import Home from "@pages/Home/Home";
-import NewProduct from "@pages/Home/NewProduct";
+import Category from "@pages/Category";
+import Home from "@pages/Home";
+import ProductRegister from "@pages/ProductRegister";
 import MyAccount from "@pages/MyAccount/MyAccount";
-import Product from "@pages/Product";
+import ProductDetail from "@pages/ProductDetail";
 import Register from "@pages/Register";
 import SalesList from "@pages/SalesList";
 import WishList from "@pages/WishList";
@@ -19,9 +19,10 @@ export const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route path={ROUTE_PATH.home} element={<Home />} />
-      <Route path={ROUTE_PATH.new} element={<NewProduct />} />
+      <Route path={ROUTE_PATH.new} element={<ProductRegister />} />
+      <Route path={ROUTE_PATH.edit} element={<ProductRegister />} />
       <Route path={ROUTE_PATH.category} element={<Category />} />
-      <Route path={ROUTE_PATH.product} element={<Product />} />
+      <Route path={ROUTE_PATH.detail} element={<ProductDetail />} />
       <Route path={ROUTE_PATH.sales} element={<SalesList />} />
       <Route path={ROUTE_PATH.wish} element={<WishList />} />
       <Route path={ROUTE_PATH.chat} element={<Chat />} />
