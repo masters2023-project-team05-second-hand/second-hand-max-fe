15e31b0ea33a22af4e414cd96817d73aeaa92f48
Jisoo Yoo <youzysu@gmail.com>
2023-09-13 18:44:51 +0900

    design: BottomBar 공통 스타일 컴포넌트 생성, Page 레이아웃 수정
    - 페이지 끝까지 스크롤하기 위해 Page height 조정

--- fe/src/styles/common.ts
+++ fe/src/styles/common.ts
@@ -1,16 +1,15 @@
 import styled from "styled-components";
 import { HEIGHT, WIDTH } from "./constants";
 
+// 참고: scroll을 Page에서 제어하고 있어서 min-height 하단바의 높이만큼 빼줘야 끝까지 스크롤 가능
 export const Page = styled.div`
   position: relative;
   margin: 0 auto;
-  max-width: ${WIDTH.page}px;
-  min-height: 100vh;
-  height: 100vh;
+  width: ${WIDTH.page}px;
+  height: calc(100vh - ${HEIGHT.navigationBar}px);
   overflow: scroll;
   background-color: ${({ theme: { color } }) => color.neutralBackground};
   box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
-  padding-bottom: ${HEIGHT.navigationBar}px;
   box-sizing: border-box;
 
   &::-webkit-scrollbar {
@@ -40,4 +39,18 @@ export const Dim = styled.div`
   height: 100%;
   background-color: ${({ theme: { color } }) => color.neutralOverlay};
   z-index: -1;
+`;
+
+export const BottomBar = styled.div`
+  display: flex;
+  align-items: center;
+  position: fixed;
+  bottom: 0;
+  background-color: ${({ theme: { color } }) => color.neutralBackgroundWeak};
+  border-top: ${({ theme: { color } }) => `1px solid ${color.neutralBorder}`};
+  width: ${WIDTH.page}px;
+  height: ${HEIGHT.navigationBar}px;
+  color: ${({ theme: { color } }) => color.neutralTextStrong};
+  font: ${({ theme: { font } }) => font.availableDefault16};
+  box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.2);
 `;
