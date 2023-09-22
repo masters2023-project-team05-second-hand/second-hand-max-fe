842cf7bd6efdf67fa9c45b0305618a4405b0ac1b
Jisoo Yoo <youzysu@gmail.com>
2023-09-22 13:29:46 +0900

    fix: 기본 동네 ID 임시 통일

--- fe/src/store/constants.ts
+++ fe/src/store/constants.ts
@@ -1,8 +1,10 @@
 export const SELLING_ID = 1;
 export const RESERVATION_ID = 2;
 export const DEFAULT_TAB = { id: 0, name: "전체" };
+
+// TODO: 현재 유저 위치 기반으로 가까운 동네로 설정하도록 개선 (API 추가 필요)
 export const DEFAULT_ADDRESS = {
-  id: 1168064000,
+  id: 1,
   name: "역삼1동",
 };
 export const SLIDE_TIME = 220;
